45a14a3e79b0da47d31c09abaad34609cc907d41
jakeahumphreys <jakehumphreys@outlook.com>
2023-01-25 19:06:18 +0000

    removed unused dependencies

--- SpotifyPlaylistDownloader/SpotifyClient.cs
+++ SpotifyPlaylistDownloader/SpotifyClient.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Net.Http.Headers;
+﻿using System.Net.Http.Headers;
 using System.Text;
 using System.Text.Json;
-using System.Threading.Tasks;
 using SpotifyPlaylistDownloader.Models.Authentication;
 using SpotifyPlaylistDownloader.Models.Common;
 using SpotifyPlaylistDownloader.Models.SpotifyApi;
